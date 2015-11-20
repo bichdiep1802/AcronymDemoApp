e1007f1362e754dce10c23bb228fd5b63d9fcd84
Bich Diep <diepb@seattleu.edu>
2015-11-20 00:19:23 -0800

    Update functions.js

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,6 +1,6 @@
-function showAcronym () {
-    var value = document.getElementById('acronymInput').value;
+document.getElementById("searchButton").addEventListener("click", function(){
+	var value = document.getElementById('acronymInput').value;
 	if (value)  
-	   document.getElementById('result').innerHTML = value;
-    }
-}
+	    document.getElementById('result').innerHTML = value;
+	
+});
