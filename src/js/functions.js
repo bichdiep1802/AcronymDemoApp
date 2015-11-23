c9afd629919498e805356622f79a88b104fdc046
Bich Diep <diepb@seattleu.edu>
2015-11-23 13:48:09 -0800

    first commit

---
+++ src/js/functions.js
@@ -0,0 +1,24 @@
+ // button handler
+ document.getElementById("searchButton").addEventListener("click", function(){
+	var acronym = document.getElementById('acronymInput').acronym;
+	var definition = lookupAcronym(acronym);
+	console.log('The meaning of ' + acronym + ' is ' + definition);
+	document.getElementById('result').innerHTML = definition;
+ });
+
+ // function to look up for acronym's def
+function lookupAcronym(acronym) {
+	if (acronym)  {
+	    var xmlhttp = new XMLHttpRequest();
+	    var url = "https://raw.githubusercontent.com/bichdiep1802/demoApp/master/src/fakedAcronyms.JSON";
+        xmlhttp.onreadystatechange=function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				var arr = JSON.parse(xmlhttp.responseText);
+        	    return arr[acronym];
+			}
+   		}
+	    xmlhttp.open("GET", url, true);
+	    xmlhttp.send();
+	}
+	return "sorry!";
+}
