8020db70bc1eb807be8e57961a6e039c796a66eb
Bich Diep <diepb@seattleu.edu>
2015-11-23 12:42:25 -0800

    Delete functions.js

--- js/functions.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("searchButton").addEventListener("click", function(){
-	var value = document.getElementById('acronymInput').value;
-	if (value)  
-	    document.getElementById('result').innerHTML = value;
-	
-});
