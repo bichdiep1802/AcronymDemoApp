efe632432e54d6fa3cfbe1f2842c145f952138f6
Bich Diep <diepb@seattleu.edu>
2015-11-24 22:22:48 -0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -10,17 +10,18 @@
 
  // function to look up for acronym's def
 function lookupAcronym(acronym) {
+	var def = "...";
 	if (acronym)  {
-	    var xmlhttp = new XMLHttpRequest();
-	    var url = "https://raw.githubusercontent.com/bichdiep1802/demoApp/master/src/fakedAcronyms.JSON";
-        xmlhttp.onreadystatechange=function() {
+	    	var xmlhttp = new XMLHttpRequest();
+	    	var url = "https://raw.githubusercontent.com/bichdiep1802/demoApp/master/src/fakedAcronyms.JSON";
+        	xmlhttp.onreadystatechange=function() {
 			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 				var arr = JSON.parse(xmlhttp.responseText);
-        	    return arr[acronym];
+        	    		def = arr[acronym];
 			}
    		}
-	    xmlhttp.open("GET", url, true);
-	    xmlhttp.send();
+	    	xmlhttp.open("GET", url, true);
+	    	xmlhttp.send();
 	}
 	return "sorry!";
 }
