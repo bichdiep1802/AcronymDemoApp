9e4810c851141c25fbb631133816a96aa8fff42d
Bich Diep <diepb@seattleu.edu>
2015-11-24 23:31:27 -0800

    Update functions.js

--- src/js/functions.js
+++ src/js/functions.js
@@ -3,26 +3,22 @@
  // button handler
  function buttonHandler() {
     	var acronym = document.getElementById("acronymInput").value;
-	var definition = lookupAcronym(acronym);
-	console.log("The meaning of " + acronym + " is " + definition);
-	document.getElementById("result").innerHTML = definition;
+	lookupAcronym(acronym);
 }
 
  // function to look up for acronym's def
 function lookupAcronym(acronym) {
-	var def = "...";
 	if (acronym)  {
 	    	var xmlhttp = new XMLHttpRequest();
 	    	var url = "src/js/fakedAcronyms.JSON";
         	xmlhttp.onreadystatechange=function() {
 			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 				var arr = JSON.parse(xmlhttp.responseText);
-        	    		def = arr[acronym];
+        	    		document.getElementById("result").innerHTML = arr[acronym];
         	    		console.log(acronym + " is " + def);
 			}
    		};
 	    	xmlhttp.open("GET", url, true);
 	    	xmlhttp.send();
 	}
-	return def;
 }
